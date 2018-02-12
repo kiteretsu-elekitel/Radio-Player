a8cc8c67026cecfc6b73148725a5e6e130dad13e
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2018-02-12 22:26:20 +0900

    Initial Commit

---
+++ Radio Player/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Radio Player
+//
+//  Created by Darjeeling on 2018/02/12.
+//  Copyright © 2018年 Darjeeling. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+
+
+}
+
