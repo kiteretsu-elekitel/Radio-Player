1ba92e7d28773f520f225c64d0995e3e8163d555
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2018-03-19 00:11:42 +0900

    support play hard corded audio file.

--- Radio Player/ViewController.swift
+++ Radio Player/ViewController.swift
@@ -7,19 +7,50 @@
 //
 
 import UIKit
+import AVFoundation
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVAudioPlayerDelegate {
 
-	override func viewDidLoad() {
-		super.viewDidLoad()
-		// Do any additional setup after loading the view, typically from a nib.
-	}
+	var player = AVAudioPlayer()
 
 	override func didReceiveMemoryWarning() {
 		super.didReceiveMemoryWarning()
 		// Dispose of any resources that can be recreated.
 	}
 
+	@IBAction func tapStartButton(_ sender: Any) {
+		player.play()
+	}
+	
+	
+	@IBAction func tapStopButton(_ sender: Any) {
+		player.pause()
+	}
+	
+	func audioPlayerDif(){
+		
+		// 音声ファイルのパスを定義 ファイル名, 拡張子を定義
+		let audioPath = Bundle.main.path(forResource: "baka_20180312", ofType: "m4a")
+		//let audioURL = URL(fileURLWithPath: audioPath!)
 
+		//ファイルが存在しない、拡張子が誤っている、などのエラーを防止するために実行テスト(try)する。
+		
+		if let path = audioPath {
+			print("ditected audio file")
+			do{
+				//tryで、ファイルが問題なければ player変数にaudioPathを定義
+				try player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: path) as URL)
+			}catch{
+				print("file type is not supporting")
+			}
+		}
+		
+	}
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		audioPlayerDif()
+		
+		// Do any additional setup after loading the view, typically from a nib.
+	}
 }
 
