4a21324541140d3cff49d321ce6093f594cf7860
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2018-05-29 19:49:55 +0900

    add Player.swift

---
+++ Radio Player/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Radio Player
+//
+//  Created by Darjeeling on 2018/05/29.
+//  Copyright © 2018年 Darjeeling. All rights reserved.
+//
+
+import Foundation
+import AVFoundation
+
+class Player {
+	
+}
