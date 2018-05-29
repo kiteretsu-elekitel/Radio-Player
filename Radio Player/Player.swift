b14ca9abc7eb24552414b16452c5dfd61ade7556
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2018-05-29 21:39:14 +0900

    add init at Player class

--- Radio Player/Player.swift
+++ Radio Player/Player.swift
@@ -10,5 +10,21 @@ import Foundation
 import AVFoundation
 
 class Player {
+	let audioPath: String?
+	var player: AVAudioPlayer
+
+	init(file: String, type: String) {
+		self.audioPath = Bundle.main.path(forResource: file, ofType: type)
+		self.player = AVAudioPlayer()
+		do {
+			try self.player = AVAudioPlayer(contentsOf: NSURL(fileURLWithPath: self.audioPath!) as URL)
+		} catch {
+			print("file type is not supporting")
+		}
+	}
+	
+	func getAudioDuration() -> Int {
+		return Int(player.duration)
+	}
 	
 }
