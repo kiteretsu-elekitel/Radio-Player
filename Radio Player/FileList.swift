49965b6f3554da98470c469f8b64f2c78db547e8
kiteretsu-elekitel <kiteretsu.elekitel@gmail.com>
2018-06-23 22:25:51 +0900

    Add FileLitst.swift

---
+++ Radio Player/FileList.swift
@@ -0,0 +1,13 @@
+//
+//  FileList.swift
+//  Radio Player
+//
+//  Created by Darjeeling on 2018/06/17.
+//  Copyright © 2018年 Darjeeling. All rights reserved.
+//
+
+import Foundation
+
+class FileList {
+	
+}
